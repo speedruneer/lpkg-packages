3cb9499beadaa72a0d3ad368b03a8ac370fb9a4d
speedruneer <101725579+speedruneer@users.noreply.github.com>
2025-09-09 12:47:41 +0200

    Update and rename source.ini to source.lua

--- source.lua
+++ source.lua
@@ -4,3 +4,7 @@ link "github.com/speedruneer/lpkg-packages"
 
 [packages]
 package "https://raw.githubusercontent.com/speedruneer/lpkg-packages/refs/heads/main/cosu-install.lua"
+
+return {
+{id="cosu-editor",install="https://raw.githubusercontent.com/speedruneer/lpkg-packages/refs/heads/main/cosu-install.lua",version=1}
+}
