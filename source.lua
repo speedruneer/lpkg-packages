e78c2e0be926796599f0d7d8f72113704b32d423
speedruneer <101725579+speedruneer@users.noreply.github.com>
2025-09-09 16:26:31 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -4,4 +4,5 @@ link="https://raw.githubusercontent.com/speedruneer/lpkg-packages/refs/heads/mai
 packages={
 {id="cosu-editor",install="https://raw.githubusercontent.com/speedruneer/lpkg-packages/refs/heads/main/cosu-install.lua",version=1},
 {id="ccfetch",install="https://raw.githubusercontent.com/speedruneer/lpkg-packages/refs/heads/main/ccfetch.lua",version=1},
+{id="cash",install="https://raw.githubusercontent.com/speedruneer/lpkg-packages/refs/heads/main/cash.lua",version=1},
 }}
