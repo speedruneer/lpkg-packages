72c26eb03b6f73c1aacccf6ec65ab46a48d0e7cb
speedruneer <101725579+speedruneer@users.noreply.github.com>
2025-09-09 12:50:51 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,10 +1,6 @@
-[source]
-name "lpkg"
-link "github.com/speedruneer/lpkg-packages"
-
-[packages]
-package "https://raw.githubusercontent.com/speedruneer/lpkg-packages/refs/heads/main/cosu-install.lua"
-
 return {
+name="lpkg"
+link="https://raw.githubusercontent.com/speedruneer/lpkg-packages/refs/heads/main/source.lua",
+packages={
 {id="cosu-editor",install="https://raw.githubusercontent.com/speedruneer/lpkg-packages/refs/heads/main/cosu-install.lua",version=1}
-}
+}}
