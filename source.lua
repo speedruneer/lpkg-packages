77a01d1e248478147d7ee73fae22f24b0d820d50
speedruneer <101725579+speedruneer@users.noreply.github.com>
2025-09-09 13:09:27 +0200

    CCFETCH added

--- source.lua
+++ source.lua
@@ -2,5 +2,6 @@ return {
 name="lpkg",
 link="https://raw.githubusercontent.com/speedruneer/lpkg-packages/refs/heads/main/source.lua",
 packages={
-{id="cosu-editor",install="https://raw.githubusercontent.com/speedruneer/lpkg-packages/refs/heads/main/cosu-install.lua",version=1}
+{id="cosu-editor",install="https://raw.githubusercontent.com/speedruneer/lpkg-packages/refs/heads/main/cosu-install.lua",version=1},
+{id="ccfetch",install="https://raw.githubusercontent.com/speedruneer/lpkg-packages/refs/heads/main/ccfetch.lua",version=1},
 }}
