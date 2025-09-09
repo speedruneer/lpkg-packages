532337097851ff9fd4f6b08ddd9e876ab19eb00b
speedruneer <101725579+speedruneer@users.noreply.github.com>
2025-09-09 12:54:35 +0200

    V2

--- cosu-install.lua
+++ cosu-install.lua
@@ -1,2 +1,1 @@
-if lpkg.api and lpkg.api.wantGetInfo then return {version=1,dependencies={"lpkgAPI>=3",},id="consult-editor"} end
 shell.run("wget https://github.com/SammyForReal/consult/releases/latest/download/cosu.lua /bin/cosu.lua")
